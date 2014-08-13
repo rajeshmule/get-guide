8b36bc9b642da38053ade120f7e78daed70ecbfe
rajeshmule <rajeshsmule@gmail.com.com>
2014-08-13 15:14:11 +0530

    Remove Top and Bottom margine

--- MyGet/src/com/rajesh/myget/MainActivity.java
+++ MyGet/src/com/rajesh/myget/MainActivity.java
@@ -36,6 +36,20 @@ public class MainActivity extends ActionBarActivity {
 		Button btnosx = (Button) findViewById(R.id.bosx);
 		Button btnwin = (Button) findViewById(R.id.bwin);
 		Button btnlin = (Button) findViewById(R.id.blin);
+		Button bt1 = (Button) findViewById(R.id.b1);
+		Button bt2 = (Button) findViewById(R.id.b2);
+		Button bt3 = (Button) findViewById(R.id.b3);
+		Button bt4 = (Button) findViewById(R.id.b4);
+		Button bt5 = (Button) findViewById(R.id.b5);
+		Button bt6 = (Button) findViewById(R.id.b6);
+		Button bt7 = (Button) findViewById(R.id.b7);
+		Button bt8 = (Button) findViewById(R.id.b8);
+		Button bt9 = (Button) findViewById(R.id.b9);
+		Button bt10 = (Button) findViewById(R.id.b10);
+		Button bt11 = (Button) findViewById(R.id.b11);
+		Button bt12 = (Button) findViewById(R.id.b12);
+		Button bt13 = (Button) findViewById(R.id.b13);
+		
 
 		tv1.setTypeface(font);
 		tv2.setTypeface(font);
@@ -84,6 +98,162 @@ public class MainActivity extends ActionBarActivity {
 						android.content.Intent.ACTION_VIEW);
 				myWebLink.setData(Uri
 						.parse("http://git-scm.com/download/linux"));
+				startActivity(myWebLink);
+			}
+		});
+		bt1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://gitx.laullon.com/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt2.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://www.git-tower.com/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt3.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://www.sourcetreeapp.com/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt4.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("https://mac.github.com/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt5.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("https://itunes.apple.com/gb/app/gitbox/id403388357?mt=12"));
+				startActivity(myWebLink);
+			}
+		});
+		bt6.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://git-scm.com/book"));
+				startActivity(myWebLink);
+			}
+		});
+		bt7.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://progit.org/book/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt8.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://think-like-a-git.net/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt9.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("https://help.github.com/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt10.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://marklodato.github.io/visual-git-guide/index-en.html"));
+				startActivity(myWebLink);
+			}
+		});
+		bt11.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("https://groups.google.com/forum/#!forum/git-users"));
+				startActivity(myWebLink);
+			}
+		});
+		bt12.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://jk.gs/git/"));
+				startActivity(myWebLink);
+			}
+		});
+		bt13.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent myWebLink = new Intent(
+						android.content.Intent.ACTION_VIEW);
+				myWebLink.setData(Uri
+						.parse("http://rogerdudler.github.io/git-guide/"));
 				startActivity(myWebLink);
 			}
 		});
