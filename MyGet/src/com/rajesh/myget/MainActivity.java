e7b9b4e7e7c4a969ddba9eaa55fbfd364c29492c
rajeshmule <rajeshsmule@gmail.com.com>
2014-08-14 21:40:31 +0530

    add Links

--- MyGet/src/com/rajesh/myget/MainActivity.java
+++ MyGet/src/com/rajesh/myget/MainActivity.java
@@ -5,6 +5,8 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -16,6 +18,8 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// set font from asset folder on this project
 
 		Typeface font = Typeface.createFromAsset(getAssets(),
 				"FantasqueSansMono-Regular.ttf");
@@ -33,43 +37,52 @@ public class MainActivity extends ActionBarActivity {
 		TextView tv11 = (TextView) findViewById(R.id.tvtitle11);
 		TextView tv12 = (TextView) findViewById(R.id.tvtitle12);
 		TextView tv13 = (TextView) findViewById(R.id.tvtitle13);
+
+		// take id for hyperlink to textview
+
+		TextView link1 = (TextView) findViewById(R.id.text1);
+		TextView link2 = (TextView) findViewById(R.id.text2);
+		TextView link3 = (TextView) findViewById(R.id.text3);
+		TextView link4 = (TextView) findViewById(R.id.text4);
+		TextView link5 = (TextView) findViewById(R.id.text5);
+		TextView link6 = (TextView) findViewById(R.id.text6);
+		TextView link7 = (TextView) findViewById(R.id.text7);
+		TextView link8 = (TextView) findViewById(R.id.text8);
+		TextView link9 = (TextView) findViewById(R.id.text9);
+		TextView link10 = (TextView) findViewById(R.id.text10);
+		TextView link11 = (TextView) findViewById(R.id.text11);
+		TextView link12 = (TextView) findViewById(R.id.text12);
+		TextView link13 = (TextView) findViewById(R.id.text13);
+
+		// take id for hyper link to button
+
 		Button btnosx = (Button) findViewById(R.id.bosx);
 		Button btnwin = (Button) findViewById(R.id.bwin);
 		Button btnlin = (Button) findViewById(R.id.blin);
-		Button bt1 = (Button) findViewById(R.id.b1);
-		Button bt2 = (Button) findViewById(R.id.b2);
-		Button bt3 = (Button) findViewById(R.id.b3);
-		Button bt4 = (Button) findViewById(R.id.b4);
-		Button bt5 = (Button) findViewById(R.id.b5);
-		Button bt6 = (Button) findViewById(R.id.b6);
-		Button bt7 = (Button) findViewById(R.id.b7);
-		Button bt8 = (Button) findViewById(R.id.b8);
-		Button bt9 = (Button) findViewById(R.id.b9);
-		Button bt10 = (Button) findViewById(R.id.b10);
-		Button bt11 = (Button) findViewById(R.id.b11);
-		Button bt12 = (Button) findViewById(R.id.b12);
-		Button bt13 = (Button) findViewById(R.id.b13);
-		
 
-		tv1.setTypeface(font);
-		tv2.setTypeface(font);
-		tv3.setTypeface(font);
-		tv4.setTypeface(font);
-		tv5.setTypeface(font);
-		tv6.setTypeface(font);
-		tv7.setTypeface(font);
-		tv8.setTypeface(font);
-		tv9.setTypeface(font);
-		tv10.setTypeface(font);
-		tv11.setTypeface(font);
-		tv12.setTypeface(font);
-		tv13.setTypeface(font);
+		// set font face and bold and italic
+
+		tv1.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv2.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv3.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv4.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv5.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv6.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv7.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv8.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv9.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv10.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv11.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv12.setTypeface(font, Typeface.BOLD_ITALIC);
+		tv13.setTypeface(font, Typeface.BOLD_ITALIC);
+
+		// when click on button go to browser
 
 		btnosx.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+
 				Intent myWebLink = new Intent(
 						android.content.Intent.ACTION_VIEW);
 				myWebLink.setData(Uri
@@ -77,23 +90,24 @@ public class MainActivity extends ActionBarActivity {
 				startActivity(myWebLink);
 			}
 		});
+
 		btnwin.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+
 				Intent myWebLink = new Intent(
 						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://msysgit.github.io/"));
+				myWebLink.setData(Uri.parse("http://msysgit.github.io/"));
 				startActivity(myWebLink);
 			}
 		});
+
 		btnlin.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+
 				Intent myWebLink = new Intent(
 						android.content.Intent.ACTION_VIEW);
 				myWebLink.setData(Uri
@@ -101,162 +115,73 @@ public class MainActivity extends ActionBarActivity {
 				startActivity(myWebLink);
 			}
 		});
-		bt1.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://gitx.laullon.com/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt2.setOnClickListener(new OnClickListener() {
+		// when click on text view go to browser
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://www.git-tower.com/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt3.setOnClickListener(new OnClickListener() {
+		String value1 = "<html><a href=\"http://gitx.laullon.com\">GitX (L) (OSX, open source)</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://www.sourcetreeapp.com/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt4.setOnClickListener(new OnClickListener() {
+		link1.setText(Html.fromHtml(value1));
+		link1.setMovementMethod(LinkMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("https://mac.github.com/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt5.setOnClickListener(new OnClickListener() {
+		String value2 = "<html><a href=\"http://www.git-tower.com\">Tower (OSX)</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("https://itunes.apple.com/gb/app/gitbox/id403388357?mt=12"));
-				startActivity(myWebLink);
-			}
-		});
-		bt6.setOnClickListener(new OnClickListener() {
+		link2.setText(Html.fromHtml(value2));
+		link2.setMovementMethod(LinkMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://git-scm.com/book"));
-				startActivity(myWebLink);
-			}
-		});
-		bt7.setOnClickListener(new OnClickListener() {
+		String value3 = "<html><a href=\"http://www.sourcetreeapp.com\">Source Tree (OSX &amp; Windows, free)</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://progit.org/book/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt8.setOnClickListener(new OnClickListener() {
+		link3.setText(Html.fromHtml(value3));
+		link3.setMovementMethod(LinkMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://think-like-a-git.net/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt9.setOnClickListener(new OnClickListener() {
+		String value4 = "<html><a href=\"https://mac.github.com\">GitHub for Mac (OSX, free)</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("https://help.github.com/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt10.setOnClickListener(new OnClickListener() {
+		link4.setText(Html.fromHtml(value4));
+		link4.setMovementMethod(LinkMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://marklodato.github.io/visual-git-guide/index-en.html"));
-				startActivity(myWebLink);
-			}
-		});
-		bt11.setOnClickListener(new OnClickListener() {
+		String value5 = "<html><a href=\"https://itunes.apple.com/gb/app/gitbox/id403388357?mt=12\">GitBox (OSX, App Store)</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("https://groups.google.com/forum/#!forum/git-users"));
-				startActivity(myWebLink);
-			}
-		});
-		bt12.setOnClickListener(new OnClickListener() {
+		link5.setText(Html.fromHtml(value5));
+		link5.setMovementMethod(LinkMovementMethod.getInstance());
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://jk.gs/git/"));
-				startActivity(myWebLink);
-			}
-		});
-		bt13.setOnClickListener(new OnClickListener() {
+		String value6 = "<html><a href=\"http://git-scm.com/book\">Git Community Book</a></html>";
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent myWebLink = new Intent(
-						android.content.Intent.ACTION_VIEW);
-				myWebLink.setData(Uri
-						.parse("http://rogerdudler.github.io/git-guide/"));
-				startActivity(myWebLink);
-			}
-		});
+		link6.setText(Html.fromHtml(value6));
+		link6.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value7 = "<html><a href=\"http://progit.org/book\">Pro Git</a></html>";
+
+		link7.setText(Html.fromHtml(value7));
+		link7.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value8 = "<html><a href=\"http://think-like-a-git.net\">Think like a git</a></html>";
+
+		link8.setText(Html.fromHtml(value8));
+		link8.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value9 = "<html><a href=\"https://help.github.com\">GitHub Help</a></html>";
+
+		link9.setText(Html.fromHtml(value9));
+		link9.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value10 = "<html><a href=\"http://marklodato.github.io/visual-git-guide/index-en.html\">A Visual Git Guide</a></html>";
+
+		link10.setText(Html.fromHtml(value10));
+		link10.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value11 = "<html><a href=\"https://groups.google.com/forum/#!forum/git-users\">Git User Mailing List</a></html>";
+
+		link11.setText(Html.fromHtml(value11));
+		link11.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value12 = "<html><a href=\"http://jk.gs/git\">#git on irc.freenode.net</a></html>";
+
+		link12.setText(Html.fromHtml(value12));
+		link12.setMovementMethod(LinkMovementMethod.getInstance());
+
+		String value13 = "<html><a href=\"http://rogerdudler.github.io/git-guide\">http://rogerdudler.github.io/git-guide/</a></html>";
+
+		link13.setText(Html.fromHtml(value13));
+		link13.setMovementMethod(LinkMovementMethod.getInstance());
 
 	}
 }
