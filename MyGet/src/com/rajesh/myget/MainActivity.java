66caff321eafa1d9080e916c4bb5685ed529e02c
rajeshmule <rajeshsmule@gmail.com.com>
2014-08-18 20:01:05 +0530

    no title bar

--- MyGet/src/com/rajesh/myget/MainActivity.java
+++ MyGet/src/com/rajesh/myget/MainActivity.java
@@ -9,6 +9,7 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,8 +18,10 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		 this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 
+	
 		// set font from asset folder on this project
 
 		Typeface font = Typeface.createFromAsset(getAssets(),
